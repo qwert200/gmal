ce25061741157ae966e7db95b3519aafe71225f3
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-17 14:10:59 +0300

    Update tringtring.py

--- VIPMUSIC/plugins/VIP/tringtring.py
+++ VIPMUSIC/plugins/VIP/tringtring.py
@@ -4,26 +4,26 @@ from VIPMUSIC import app
 from pyrogram import filters
 from pyrogram.types import InputMediaPhoto
 
-@app.on_message(filters.command(["image"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
+@app.on_message(filters.command(["صور"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
 async def pinterest(_, message):
      chat_id = message.chat.id
 
      try:
        query= message.text.split(None,1)[1]
      except:
-         return await message.reply("**ɢɪᴠᴇ ɪᴍᴀɢᴇ ɴᴀᴍᴇ ғᴏʀ sᴇᴀʀᴄʜ 🔍**")
+         return await message.reply("**🚦اكتب شيء للبحث عنه... ")
 
      images = get(f"https://pinterest-api-one.vercel.app/?q={query}").json()
 
      media_group = []
      count = 0
 
-     msg = await message.reply(f"sᴄʀᴀᴘɪɴɢ ɪᴍᴀɢᴇs ғʀᴏᴍ ᴘɪɴᴛᴇʀᴇᴛs...")
+     msg = await message.reply(f"🚦انتظر قليلا جاࢪي البحث... ")
      for url in images["images"][:6]:
                   
           media_group.append(InputMediaPhoto(media=url))
           count += 1
-          await msg.edit(f"=> ᴏᴡᴏ sᴄʀᴀᴘᴇᴅ ɪᴍᴀɢᴇs {count}")
+          await msg.edit(f"🚦عدد الصور المطابقه {count}")
 
      try:
         
@@ -35,4 +35,4 @@ async def pinterest(_, message):
 
      except Exception as e:
            await msg.delete()
-           return await message.reply(f"ᴇʀʀᴏʀ : {e}")
+           return await message.reply(f"خطأ : {e}")
