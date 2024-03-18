ca59c4c9b2a299b586136012fcd61c1be0e37a0c
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-18 19:16:15 +0300

    Update queue.py

--- VIPMUSIC/plugins/tools/queue.py
+++ VIPMUSIC/plugins/tools/queue.py
@@ -36,7 +36,7 @@ def get_duration(playing):
 
 
 @app.on_message(
-    filters.command(["queue", "cqueue", "player", "cplayer", "playing", "cplaying"])
+    filters.command(["queue", "cqueue", "player", "cplayer","قائمه التشغيل", "قائمة التشغيل"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) 
     & filters.group
     & ~BANNED_USERS
 )
