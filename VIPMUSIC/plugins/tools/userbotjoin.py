3235d077a3933626d720c0c647c75e04c86a71f2
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-18 22:29:48 +0300

    Update userbotjoin.py

--- VIPMUSIC/plugins/tools/userbotjoin.py
+++ VIPMUSIC/plugins/tools/userbotjoin.py
@@ -21,12 +21,12 @@ from VIPMUSIC.utils.database import get_assistant, is_active_chat
 links = {}
 
 
-@app.on_message(filters.group & filters.command(["userbotjoin", f"userbotjoin@{app.username}"]) & ~filters.private)
+@app.on_message(filters.group & filters.command(["userbotjoin", f"userbotjoin@{app.username}","انضم"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~filters.private)
 async def join_group(client, message):
     chat_id = message.chat.id
     userbot = await get_assistant(message.chat.id)
     userbot_id = userbot.id
-    done = await message.reply("**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ɪɴᴠɪᴛɪɴɢ ᴀssɪsᴛᴀɴᴛ**...")
+    done = await message.reply("**🚦انتظر قليلا من فضلك وسوف ينضم الحساب المساعد الى المجموعه...**")
     await asyncio.sleep(1)
     # Get chat member object
     chat_member = await app.get_chat_member(chat_id, app.id)
@@ -35,16 +35,16 @@ async def join_group(client, message):
     if message.chat.username and not chat_member.status == ChatMemberStatus.ADMINISTRATOR:
         try:
             await userbot.join_chat(message.chat.username)
-            await done.edit_text("**✅ ᴀssɪsᴛᴀɴᴛ ᴊᴏɪɴᴇᴅ.**")
+            await done.edit_text("**🚦تم انضمام الحساب المساعد بنجاح⚡♥**")
         except Exception as e:
-            await done.edit_text("**ɪ ɴᴇᴇᴅ ᴀᴅᴍɪɴ ᴘᴏᴡᴇʀ ᴛᴏ ᴜɴʙᴀɴ ɪɴᴠɪᴛᴇ ᴍʏ ᴀssɪsᴛᴀɴᴛ!**")
+            await done.edit_text("**🚦امنحني جميع الصلاحيات المشرف لكي اقوم باضافة الحساب المساعد...**")
             
 
     # Condition 2: Group username is present, bot is admin, and Userbot is not banned
     if message.chat.username and chat_member.status == ChatMemberStatus.ADMINISTRATOR:
         try:
             await userbot.join_chat(message.chat.username)
-            await done.edit_text("**✅ ᴀssɪsᴛᴀɴᴛ ᴊᴏɪɴᴇᴅ.**")
+            await done.edit_text("**🚦تم انضمام الحساب المساعد بنجاح⚡♥**")
         except Exception as e:
             await done.edit_text(str(e))
 
@@ -56,16 +56,16 @@ async def join_group(client, message):
         if userbot_member.status in [ChatMemberStatus.BANNED, ChatMemberStatus.RESTRICTED]:
             try:
                 await app.unban_chat_member(chat_id, userbot.id)
-                await done.edit_text("**ᴀssɪsᴛᴀɴᴛ ɪs ᴜɴʙᴀɴɴɪɴɢ...**")
+                await done.edit_text("**🚦الحساب المساعد محظور...**")
                 await userbot.join_chat(message.chat.username)
-                await done.edit_text("**ᴀssɪsᴛᴀɴᴛ ᴡᴀs ʙᴀɴɴᴇᴅ, ʙᴜᴛ ɴᴏᴡ ᴜɴʙᴀɴɴᴇᴅ, ᴀɴᴅ ᴊᴏɪɴᴇᴅ ᴄʜᴀᴛ ✅**")
+                await done.edit_text("**🚦قم بارالة الحساب المساعد من قائمة المحظورين وحاول مره اخرى**")
             except Exception as e:
-                await done.edit_text("**ғᴀɪʟᴇᴅ ᴛᴏ ᴊᴏɪɴ, ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ʙᴀɴ ᴘᴏᴡᴇʀ ᴀɴᴅ ɪɴᴠɪᴛᴇ ᴜsᴇʀ ᴘᴏᴡᴇʀ ᴏʀ ᴜɴʙᴀɴ ᴀssɪsᴛᴀɴᴛ ᴍᴀɴᴜᴀʟʟʏ ᴛʜᴇɴ ᴛʀʏ ᴀɢᴀɪɴ ʙʏ /userbotjoin**")
+                await done.edit_text("**🚦من فضلك قم بازالة الحساب المساعد من قائمة المحظورين وامنح البوت جميع الصلاحيات وحاول مرة اخرى.**")
         return
     
     # Condition 4: Group username is not present/group is private, bot is not admin
     if not message.chat.username and not chat_member.status == ChatMemberStatus.ADMINISTRATOR:
-        await done.edit_text("**ɪ ɴᴇᴇᴅ ᴀᴅᴍɪɴ ᴘᴏᴡᴇʀ ᴛᴏ ɪɴᴠɪᴛᴇ ᴍʏ ᴀssɪsᴛᴀɴᴛ.**")
+        await done.edit_text("**🚦ارفعني مشرف في المجموعه لاستطيع اضافة الحساب المساعد.**")
         
 
 
@@ -75,17 +75,17 @@ async def join_group(client, message):
             try:
                 userbot_member = await app.get_chat_member(chat_id, userbot.id)
                 if userbot_member.status not in [ChatMemberStatus.BANNED, ChatMemberStatus.RESTRICTED]:
-                    await done.edit_text("**✅ ᴀssɪsᴛᴀɴᴛ ᴀʟʀᴇᴀᴅʏ ᴊᴏɪɴᴇᴅ.**")
+                    await done.edit_text("**🚦الحساب المساعد موجود بالفعل.**")
                     return
             except Exception as e:
-                await done.edit_text("**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ɪɴᴠɪᴛɪɴɢ ᴀssɪsᴛᴀɴᴛ**.")
-                await done.edit_text("**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ɪɴᴠɪᴛɪɴɢ ᴀssɪsᴛᴀɴᴛ**...")
+                await done.edit_text("**🚦انتظر قليلا لاضافة الحساب المساعد.**")
+                await done.edit_text("**🚦انتظر قليلا لاضافة الحساب المساعد.**")
                 invite_link = await app.create_chat_invite_link(chat_id, expire_date=None)
                 await asyncio.sleep(2)
                 await userbot.join_chat(invite_link.invite_link)
-                await done.edit_text("**✅ ᴀssɪsᴛᴀɴᴛ ᴊᴏɪɴᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.**")
+                await done.edit_text("**🚦تم انضمام الحساب المساعد بنجاح.**")
         except Exception as e:
-            await done.edit_text(f"**➻ ᴀᴄᴛᴜᴀʟʟʏ ɪ ғᴏᴜɴᴅ ᴛʜᴀᴛ ᴍʏ ᴀssɪsᴛᴀɴᴛ ʜᴀs ɴᴏᴛ ᴊᴏɪɴ ᴛʜɪs ɢʀᴏᴜᴘ ᴀɴᴅ ɪ ᴀᴍ ɴᴏᴛ ᴀʙʟᴇ ᴛᴏ ɪɴᴠɪᴛᴇ ᴍʏ ᴀssɪsᴛᴀɴᴛ ʙᴇᴄᴀᴜsᴇ [ ɪ ᴅᴏɴᴛ ʜᴀᴠᴇ  ɪɴᴠɪᴛᴇ ᴜsᴇʀ ᴀᴅᴍɪɴ ᴘᴏᴡᴇʀ ] sᴏ ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴍᴇ ɪɴᴠɪᴛᴇ ᴜsᴇʀs ᴀᴅᴍɪɴ ᴘᴏᴡᴇʀ ᴛʜᴇɴ ᴛʀʏ ᴀɢᴀɪɴ ʙʏ- /userbotjoin.**\n\n**➥ ɪᴅ »** @{userbot.username}")
+            await done.edit_text(f"**🚦الحساب المساعد غير موجود في المجموعه وغير قادر على اضافته\n امنحني جميع الصلاحيات وحاول مرة اخرى.**")
 
     
     
@@ -95,13 +95,13 @@ async def join_group(client, message):
         if userbot_member.status in [ChatMemberStatus.BANNED, ChatMemberStatus.RESTRICTED]:
             try:
                 await app.unban_chat_member(chat_id, userbot.id)
-                await done.edit_text("**ᴀssɪsᴛᴀɴᴛ ɪs ᴜɴʙᴀɴɴᴇᴅ**\n**ᴛʏᴘᴇ ᴀɢᴀɪɴ:- /userbotjoin.**")
+                await done.edit_text("**🚦الحساب المساعد محظر ارفع الحظر عنه وحاول مره اخرى..**")
                 invite_link = await app.create_chat_invite_link(chat_id, expire_date=None)
                 await asyncio.sleep(2)
                 await userbot.join_chat(invite_link.invite_link)
-                await done.edit_text("**ᴀssɪsᴛᴀɴᴛ ᴡᴀs ʙᴀɴɴᴇᴅ, ɴᴏᴡ ᴜɴʙᴀɴɴᴇᴅ, ᴀɴᴅ ᴊᴏɪɴᴇᴅ ᴄʜᴀᴛ✅**")
+                await done.edit_text("**🚦الحساب المسعد محظر ارفع الحظر وسوف ينضم للمجموعه**")
             except Exception as e:
-                await done.edit_text(f"**➻ ᴀᴄᴛᴜᴀʟʟʏ ɪ ғᴏᴜɴᴅ ᴛʜᴀᴛ ᴍʏ ᴀssɪsᴛᴀɴᴛ ɪs ʙᴀɴɴᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ ᴀɴᴅ ɪ ᴀᴍ ɴᴏᴛ ᴀʙʟᴇ ᴛᴏ ᴜɴʙᴀɴ ᴍʏ ᴀssɪsᴛᴀɴᴛ ʙᴇᴄᴀᴜsᴇ [ ɪ ᴅᴏɴᴛ ʜᴀᴠᴇ  ʙᴀɴ ᴘᴏᴡᴇʀ ] sᴏ ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴍᴇ ʙᴀɴ ᴘᴏᴡᴇʀ ᴏʀ ᴜɴʙᴀɴ ᴍʏ ᴀssɪsᴛᴀɴᴛ ᴍᴀɴᴜᴀʟʟʏ ᴛʜᴇɴ ᴛʀʏ ᴀɢᴀɪɴ ʙʏ- /userbotjoin.**\n\n**➥ ɪᴅ »** @{userbot.username}")
+                await done.edit_text(f"**🚦الحساب المساعد غير موجود في المجموعه وغير قادر على اضافته\n امنحني جميع الصلاحيات وحاول مرة اخرى.**")
         return
     
     
@@ -109,42 +109,42 @@ async def join_group(client, message):
 
 
         
-@app.on_message(filters.command("userbotleave") & filters.group & admin_filter)
+@app.on_message(filters.command(["userbotleave","غادر"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group & admin_filter)
 async def leave_one(client, message):
     try:
         userbot = await get_assistant(message.chat.id)
         await userbot.leave_chat(message.chat.id)
-        await app.send_message(message.chat.id, "**✅ ᴜsᴇʀʙᴏᴛ sᴜᴄᴄᴇssғᴜʟʟʏ ʟᴇғᴛ ᴛʜɪs Chat.**")
+        await app.send_message(message.chat.id, "**🚦غادر الحساب المساعد بنجاح.**")
     except Exception as e:
         print(e)
 
 
-@app.on_message(filters.command(["leaveall", f"leaveall@{app.username}"]) & SUDOERS)
+@app.on_message(filters.command(["leaveall", f"leaveall@{app.username}","مغادره"]) & SUDOERS)
 async def leave_all(client, message):
     if message.from_user.id not in SUDOERS:
         return
 
     left = 0
     failed = 0
-    lol = await message.reply("🔄 **ᴜsᴇʀʙᴏᴛ** ʟᴇᴀᴠɪɴɢ ᴀʟʟ ᴄʜᴀᴛs !")
+    lol = await message.reply("**🚦تم المغادره بنجاح...**")
     try:
         userbot = await get_assistant(message.chat.id)
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1001733534088:
+            if dialog.chat.id == -1001420714100:
                 continue
             try:
                 await userbot.leave_chat(dialog.chat.id)
                 left += 1
                 await lol.edit(
-                    f"**ᴜsᴇʀʙᴏᴛ ʟᴇᴀᴠɪɴɢ ᴀʟʟ ɢʀᴏᴜᴘ...**\n\n**ʟᴇғᴛ:** {left} ᴄʜᴀᴛs.\n**ғᴀɪʟᴇᴅ:** {failed} ᴄʜᴀᴛs."
+                    f"**🚦جاࢪي المغاده من كل المجموعات...**\n\n**🚦النجاح:** {left} مجموعه.\n**🚦الفشل:** {failed} مجموعه.**"
                 )
             except BaseException:
                 failed += 1
                 await lol.edit(
-                    f"**ᴜsᴇʀʙᴏᴛ ʟᴇᴀᴠɪɴɢ...**\n\n**ʟᴇғᴛ:** {left} chats.\n**ғᴀɪʟᴇᴅ:** {failed} chats."
+                    f"**🚦جاࢪي المغادره من كل المجموعات...**\n\n**🚦النجاح:** {left} مجموعه.\n**🚦الفشل:** {failed} مجموعه.**"
                 )
             await asyncio.sleep(3)
     finally:
         await app.send_message(
-            message.chat.id, f"**✅ ʟᴇғᴛ ғʀᴏᴍ:* {left} chats.\n**❌ ғᴀɪʟᴇᴅ ɪɴ:** {failed} chats."
+            message.chat.id, f"**🚦تم المغادره من:* {left} chats.\n**🚦تم فشل المغادره من:** {failed} مجموعه.**"
         )
